f635d06791f6fbade87c2e6d059c5cd3c21feade
Samuel Li <samuel.li@utexas.edu>
2026-01-27 02:09:27 -0600

    notation and documentaiton fix

--- lab1_yash/jobs.c
+++ lab1_yash/jobs.c
@@ -5,8 +5,8 @@
 
 job* jobs_list[20] = {NULL}; // intilize to null ie no present jobs
 
-void add_job(int pgid, int status, char* job_name) {
-	for (int i = 0; i < 20; ++i) {
+void add_job(int pgid, int status, char* job_name) { // add job to array of 20 jobs
+	for (int i = 0; i < 20; i++) {
 		if (jobs_list[i] == NULL) {
 			job* j = malloc(sizeof(job));
 			j->job_id = i + 1;
@@ -22,9 +22,9 @@ void add_job(int pgid, int status, char* job_name) {
 		}
 	}
 }
-
+// remove job from jobs list based on job id
 void remove_job(int job_id) {
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 20; i++) {
 		if (jobs_list[i] && jobs_list[i]->job_id == job_id) {
 			free(jobs_list[i]->job_name);
 			free(jobs_list[i]);
